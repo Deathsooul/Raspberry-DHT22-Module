19b4f48c09113f571a332aa2a76de0e43e442c5b
Wallace <wbescrovaine@gmail.com>
2020-12-02 14:14:37 -0300

    Torcando pino

--- sensor.py
+++ sensor.py
@@ -1,10 +1,9 @@
-import time
 import Adafruit_DHT
-
-DHT_SENSOR= Adafruit_DHT.DHT22
-
-DHT_PIN=4
-
+import time
+ 
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 4
+ 
 while True:
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
